b9179d9d99363cf56ce9492d29ea790133ff129a
hi-the-swan <jason.portenoy@gmail.com>
2019-12-18 10:55:41 -0800

    script to collect paper sets based on the references of one starting paper

--- scripts/get_references_and_collect.py
+++ scripts/get_references_and_collect.py
@@ -27,7 +27,7 @@ from autoreview.config import Config
 
 class PaperCollector(object):
 
-    def __init__(self, config, basedir, paper_id, citations, papers, sample_size, random_seed, id_colname, cited_colname):
+    def __init__(self, config, basedir, paper_id, citations, papers, sample_size, id_colname, cited_colname):
         """
 
         :config: Config object
@@ -40,7 +40,6 @@ class PaperCollector(object):
         self.citations = citations
         self.papers = papers
         self.sample_size = sample_size
-        self.random_seed = random_seed
         self.id_colname = id_colname
         self.cited_colname = cited_colname
 
@@ -67,42 +66,61 @@ class PaperCollector(object):
         sdf_citations = load_spark_dataframe(self.citations, self.spark)
         logger.debug("collecting references of paper ID: {}".format(paper_id))
         reference_ids = sdf_citations[sdf_citations[self.id_colname]==paper_id]
-        logger.debug("count is {}".format(reference_ids.count()))
         reference_ids = reference_ids.toPandas()
         reference_ids = reference_ids[self.cited_colname].tolist()
         return reference_ids
 
     def main(self, args):
-        reference_ids = self.get_reference_ids(self.paper_id)
-        outfile = self.outdir.joinpath('reference_ids.csv')
-        logger.debug("writing {} reference ids to {}".format(len(reference_ids), outfile))
-        outfile.write_text("\n".join(reference_ids))
-        # Autoreview object will make a new Config, so tear down the existing one
-        # TODO: allow Autoreview to be passed an existing Config
-        self._config.teardown()
-        a = Autoreview(id_list=reference_ids,
-                        citations=self.citations,
-                        papers=self.papers,
-                        outdir=self.outdir,
-                        sample_size=self.sample_size,
-                        random_seed=self.random_seed,
-                        id_colname=self.id_colname,
-                        cited_colname=self.cited_colname)
-        a.get_papers_2_degrees_out()
+        logfile = self.outdir.joinpath('collect.log').open(mode='a', buffering=1)
+        logfile.write("{} - starting collection\n".format(datetime.now()))
+        logfile.write(" ".join(sys.argv))
+        logfile.write("\n")
+        try:
+            reference_ids = self.get_reference_ids(self.paper_id)
+            outfile = self.outdir.joinpath('reference_ids.csv')
+            logger.debug("writing {} reference ids to {}".format(len(reference_ids), outfile))
+            logfile.write("writing {} reference ids to {}".format(len(reference_ids), outfile))
+            outfile.write_text("\n".join(reference_ids))
+
+            # collect paper sets for each random seed (1-5):
+            for random_seed in range(1, 6):
+                this_outdir = self.outdir.joinpath('seed{:03d}'.format(random_seed))
+                if this_outdir.is_dir():
+                    logfile.write("directory {} exists. skipping".format(this_outdir))
+                    continue
+                this_outdir.mkdir()
+                logger.debug("collecting papers to go in {}".format(this_outdir))
+                logfile.write("collecting papers to go in {}\n".format(this_outdir))
+                a = Autoreview(id_list=reference_ids,
+                                citations=self.citations,
+                                papers=self.papers,
+                                outdir=this_outdir,
+                                sample_size=self.sample_size,
+                                random_seed=random_seed,
+                                id_colname=self.id_colname,
+                                cited_colname=self.cited_colname,
+                                config=self._config)
+                a.get_papers_2_degrees_out()
+
+            logfile.write("\n{} - COLLECTION COMPLETED\n".format(datetime.now()))
+        finally:
+            logfile.close()
 
 
 def main(args):
     config = Config()
-    pc = PaperCollector(config,
-                basedir = args.basedir,
-                paper_id = args.paper_id,
-                citations=args.citations,
-                papers=args.papers,
-                sample_size=args.sample_size,
-                random_seed=args.random_seed,
-                id_colname=args.id_colname,
-                cited_colname=args.cited_colname)
-    pc.main(args)
+    try:
+        pc = PaperCollector(config,
+                    basedir = args.basedir,
+                    paper_id = args.paper_id,
+                    citations=args.citations,
+                    papers=args.papers,
+                    sample_size=args.sample_size,
+                    id_colname=args.id_colname,
+                    cited_colname=args.cited_colname)
+        pc.main(args)
+    finally:
+        pc._config.teardown()
 
 
 if __name__ == "__main__":
@@ -117,7 +135,6 @@ if __name__ == "__main__":
     parser.add_argument("--citations", help="citations data (to be read by spark)")
     parser.add_argument("--papers", help="papers/cluster data (to be read by spark)")
     parser.add_argument("--sample-size", type=int, default=200, help="number of articles to sample from the set to use to train the model (integer, default: 200)")
-    parser.add_argument("--random-seed", type=int, default=999, help="random seed (integer, default: 999)")
     parser.add_argument("--id-colname", default='UID', help="column name for paper id (default: \"UID\")")
     parser.add_argument("--cited-colname", default='cited_UID', help="column name for cited paper id (default: \"cited_UID\")")
     parser.add_argument("--debug", action='store_true', help="output debugging info")

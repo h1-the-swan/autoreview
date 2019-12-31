6bada8766e89f7ec505f966e9040bfe0e69ebb36
hi-the-swan <jason.portenoy@gmail.com>
2019-12-30 22:19:06 -0800

    increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='autoreview',
-    version='0.1.0',
+    version='0.2.0',
     description='Library for ranking relevant papers based on a set of seed papers',
     long_description=readme,
     author='Jason Portenoy',
